38fceb5a359ecbd18a039813470e8f8c3cfba400
pedroberro <berrotaran.pedro@gmail.com>
2025-10-07 09:34:42 -0300

    clases nuevas

---
+++ src/main/java/org/example/model/postgres/User.java
@@ -0,0 +1,19 @@
+// package org.tp.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String fullName;
+    @Column(unique = true)
+    private String email;
+    private String passwordHash;
+    private String status;
+    private java.time.OffsetDateTime registeredAt;
+}
