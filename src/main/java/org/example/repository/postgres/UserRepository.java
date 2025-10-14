934cb9a8acd7bf1f199d5b533aa1ac71cc74da62
pedroberro <berrotaran.pedro@gmail.com>
2025-10-14 10:35:55 -0300

    agrego repositorios de postgres

--- src/main/java/org/example/repository/postgres/UserRepository.java
+++ src/main/java/org/example/repository/postgres/UserRepository.java
@@ -1,9 +1,9 @@
 package org.example.repository.postgres;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.example.model.postgres.User;
 import java.util.Optional;
-import org.example.model.postgres.*;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
-  Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
