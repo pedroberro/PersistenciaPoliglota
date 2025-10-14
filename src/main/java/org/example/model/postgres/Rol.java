767700d49632de076fe761669af47e6a6f018a5c
pedroberro <berrotaran.pedro@gmail.com>
2025-10-14 10:15:45 -0300

    elimino clases duplicadas y completo repositories mongodb

--- src/main/java/org/example/model/postgres/Rol.java
+++ src/main/java/org/example/model/postgres/Rol.java
@@ -1,3 +1,4 @@
+package org.example.model.postgres;
 
 import jakarta.persistence.*;
 import lombok.*;
--- src/main/java/org/example/repository/postgres/UserRepository.java
+++ src/main/java/org/example/repository/postgres/UserRepository.java
@@ -2,7 +2,7 @@ package org.example.repository.postgres;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-import org.example.model.*;
+import org.example.model.postgres.*;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
   Optional<User> findByEmail(String email);
