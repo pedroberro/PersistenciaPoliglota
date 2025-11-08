4c64cba5554944fea2ee093fcb3594bb7a055514
Nicolas <nmassimino1970@gmail.com>
2025-11-08 20:55:34 -0300

    Agrego excepcion en GlobalExceptionHandler

--- src/main/java/org/example/controller/GlobalExceptionHandler.java
+++ src/main/java/org/example/controller/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package org.example.controller;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
-import java.util.HashMap;
-import java.util.Map;
+import jakarta.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,4 +29,14 @@ public class GlobalExceptionHandler {
         err.put("error", ex.getMessage() != null ? ex.getMessage() : "internal error");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
     }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFound(EntityNotFoundException ex, WebRequest request) {
+    Map<String,Object> error = Map.of(
+        "timestamp", LocalDateTime.now(),
+        "message", ex.getMessage(),
+        "path", request.getDescription(false)
+    );
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+}
 }
