c89bee338bdf6cbdaf5f7a66b72d02eb868c0d65
leslinoeli <leslienoeliaflores@gmail.com>
2025-11-16 21:57:59 -0300

    Modificacioness

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,23 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Aplicación principal de PersistenciaPoliglota
- *
- * - PostgreSQL para datos relacionales
- * - MongoDB para datos de sensores y mediciones
- * - Redis para cache y sesiones
  */
-@SpringBootApplication   // Spring escanea TODO org.example.* solo con esto
+@SpringBootApplication
 @EnableCaching
 @EnableAsync
+// ✅ Solo estos paquetes se usan como repositorios JPA
+@EnableJpaRepositories(basePackages = {
+        "org.example.model.postgres",
+        "org.example.repository.postgres"
+})
+// ✅ Repositorios Mongo
+@EnableMongoRepositories(basePackages = "org.example.repository.mongodb")
 public class Main {
 
     public static void main(String[] args) {
